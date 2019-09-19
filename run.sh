0c259dbf5ce4e6ceb426e26bc15dc23e9f3bfdda
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-09-19 18:49:03 +0000

    Refact code for new args

--- run.sh
+++ run.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
-## Objetive: Download the input data for processing
-## For example: $0 landsat_5 215065 2004-12-01 /tmp/output /tmp/metadata
+## This script downloads the input data for later processing.
 
 ## Checking args
-if [ $# -ne 5 ]
+if [ $# -ne 4 ]
 then
-  echo "Usage: $0 landsat_X PPPRRR YYYY-MM-DD output_path metadata_path"
+  echo "Usage: $0 /tmp/teste landsat_X PPPRRR YYYY-MM-DD"
   exit 1
 fi
 
-## Capture args
-IMAGE_DATASET=$1
-IMAGE_PATHROW=$2
-IMAGE_DATE=$3
-OUTPUT_DIR_PATH=$4
-METADATA_DIR_PATH=$5
+## args
+ROOT_DIR=$1
+IMAGE_DATASET=$2
+IMAGE_PATHROW=$3
+IMAGE_DATE=$4
 
-# Global variables
+# global variables
 SANDBOX=$(pwd)
 
+# folders
+INPUTDOWNLOAD_DIR_PATH=$ROOT_DIR/inputdownload
+INPUTDOWNLOAD_DATA_DIR_PATH=$INPUTDOWNLOAD_DIR_PATH/data
+INPUTDOWNLOAD_METADATA_DIR_PATH=$INPUTDOWNLOAD_DIR_PATH/metadata
+
 echo "Step 1. Download image and generate station data"
-python download.py $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $OUTPUT_DIR_PATH
+python $SANDBOX/download.py $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $INPUTDOWNLOAD_DATA_DIR_PATH
 PROCESS_OUTPUT=$?
 
 if [ $PROCESS_OUTPUT -eq 0 ]
 then
   echo "Step 2. Generate metadata"
-  bash $SANDBOX/generate_metadata.sh $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $OUTPUT_DIR_PATH $METADATA_DIR_PATH
+  bash $SANDBOX/generate_metadata.sh $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $INPUTDOWNLOAD_DATA_DIR_PATH $INPUTDOWNLOAD_METADATA_DIR_PATH
 fi
 
 exit $PROCESS_OUTPUT
 
 ## Exit code
-# This script should have the following return pattern:
-# - `0` represents a successful execution.
-# - `3` represents that the Landsat image was not found.
-# And any other exit code will be considered failed.
+# exit code `0` indicates a successful execution. Any other number indicates failure.
+# In particular, `3` indicates that a Landsat image was not found for the given paramenters.
