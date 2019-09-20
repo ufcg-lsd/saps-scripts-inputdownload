d20bfef90f48530215602af4f1b5600fa49407cc
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-09-20 12:56:56 +0000

    Add script run

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## This script downloads the input data for later processing.
+
+## Checking args
+if [ $# -ne 4 ]
+then
+  echo "Usage: $0 /tmp/teste landsat_X PPPRRR YYYY-MM-DD"
+  exit 1
+fi
+
+## args
+ROOT_DIR=$1
+IMAGE_DATASET=$2
+IMAGE_PATHROW=$3
+IMAGE_DATE=$4
+
+# global variables
+SANDBOX=$(pwd)
+
+# folders
+INPUTDOWNLOADING_DIR_PATH=$ROOT_DIR/inputdownloading
+
+echo "Step 1. Download image and generate station data"
+java -Dlog4j.configuration=file:/home/ubuntu/config/log4j.properties -jar /home/ubuntu/USGS.jar $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $INPUTDOWNLOADING_DIR_PATH $INPUTDOWNLOADING_DIR_PATH
+PROCESS_OUTPUT=$?
+
+if [ $PROCESS_OUTPUT -eq 0 ]
+then
+  echo "Step 2. Generate metadata"
+  bash $SANDBOX/generate_metadata.sh $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $INPUTDOWNLOADING_DIR_PATH
+fi
+
+exit $PROCESS_OUTPUT
+
+## Exit code
+# exit code `0` indicates a successful execution. Any other number indicates failure.
+# In particular, `3` indicates that a Landsat image was not found for the given paramenters.
