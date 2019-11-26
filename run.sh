e46c0871492d0c17f4d163142d5c0eb2a0c9e37a
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-11-26 14:35:14 +0000

    Update run.sh for capture and check exit code at every step

--- run.sh
+++ run.sh
@@ -21,20 +21,51 @@ SANDBOX=$(pwd)
 # folders
 INPUTDOWNLOADING_DIR_PATH=$ROOT_DIR/inputdownloading
 
-echo "Step 1. Download image and generate station data"
+echo "Step 1. Download image"
 python $SANDBOX/download.py $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $INPUTDOWNLOADING_DIR_PATH
 PROCESS_OUTPUT=$?
 
-if [ $PROCESS_OUTPUT -eq 0 ]
+if [ ! $PROCESS_OUTPUT -eq 0 ]
 then
-  echo "Step 2. Download raster elevation"
-  wget -P $INPUTDOWNLOADING_DIR_PATH https://github.com/ThiagoWhispher/elevation-raster/raw/master/$IMAGE_PATHROW.tif
-
-  echo "Step 3. Generate metadata"
-  bash $SANDBOX/generate_metadata.sh $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $INPUTDOWNLOADING_DIR_PATH
+  echo "ERROR - Step 1. Download image [EXIT CODE = $PROCESS_OUTPUT]"
+  exit $PROCESS_OUTPUT
 fi
 
-exit $PROCESS_OUTPUT
+echo "Step 2. Generate station data"
+FILE_MTL=($INPUTDOWNLOADING_DIR_PATH/*_MTL.txt)
+FILENAME="${FILE_MTL##*/}"
+FILENAME_WITHOUT_EXTENSION="${FILENAME%_MTL*}"
+
+bash $SANDBOX/get-station-data.sh $FILENAME_WITHOUT_EXTENSION $INPUTDOWNLOADING_DIR_PATH
+PROCESS_OUTPUT=$?
+
+if [ ! $PROCESS_OUTPUT -eq 0 ]
+then
+  echo "ERROR - Step 2. Generate station data [EXIT CODE = $PROCESS_OUTPUT]"
+  exit $PROCESS_OUTPUT
+fi
+
+echo "Step 3. Download raster elevation"
+wget -P $INPUTDOWNLOADING_DIR_PATH https://github.com/ThiagoWhispher/elevation-raster/raw/master/$IMAGE_PATHROW.tif
+PROCESS_OUTPUT=$?
+
+if [ ! $PROCESS_OUTPUT -eq 0 ]
+then
+  echo "ERROR - Step 3. Download raster elevation [EXIT CODE = $PROCESS_OUTPUT]"
+  exit $PROCESS_OUTPUT
+fi
+
+echo "Step 4. Generate metadata"
+bash $SANDBOX/generate_metadata.sh $IMAGE_DATASET $IMAGE_PATHROW $IMAGE_DATE $INPUTDOWNLOADING_DIR_PATH
+PROCESS_OUTPUT=$?
+
+if [ ! $PROCESS_OUTPUT -eq 0 ]
+then
+  echo "ERROR - Step 4. Generate metadata [EXIT CODE = $PROCESS_OUTPUT]"
+  exit $PROCESS_OUTPUT
+fi
+
+exit 0
 
 ## Exit code
 # exit code `0` indicates a successful execution. Any other number indicates failure.
